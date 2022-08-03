beca60fcdce48cc936f3618cfed8e1fa18e924e3
RaissaSabino <raissa-sabino@hotmail.com>
2022-08-02 21:45:31 -0300

    Metodo de adicao de strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,25 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
         funcaoSoma(2, 2);
         funcaoSubtrair(5,2);
+        List<String> x = nomesDeAlunos("Lucas","Fernando");
+        for (String s: x){
+            System.out.println(s);
+        }
     }
 
+    public  static List<String> nomesDeAlunos(String s,String str) {
+        List<String> listString = new ArrayList<>();
+        listString.add(s);
+        listString.add(str);
+        return  listString;
 
+    }
     public static void funcaoSoma(int x, int y){
         int z = x+y;
         System.out.println("A soma de " + x + " mais " + y + " é igual a: " + z);
