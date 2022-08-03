c9a1513e0492be6673bbbd04211bf44454a53f91
RaissaSabino <raissa-sabino@hotmail.com>
2022-08-02 21:20:30 -0300

    segunda funcao

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         funcaoSoma(2, 2);
+        funcaoSubtrair(5,2);
     }
+
 
     public static void funcaoSoma(int x, int y){
         int z = x+y;
         System.out.println("A soma de " + x + " mais " + y + " é igual a: " + z);
+    }
+    public static  void funcaoSubtrair (double x,double y){
+        double z = x - y;
     }
 }
